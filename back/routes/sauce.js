f00a74461a83720e92857b93c1450570b490fcb2
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-11-20 14:04:59 +0100

    .

--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const sauceController = require('../controllers/sauce.controller');
-const authentification = require('../middleware/authentification');
+const authentication = require('../middleware/authentification');
 const multer = require('../middleware/multer');
 
-router.post('/', authentification, multer, sauceController.createSauce);
-router.get('/', authentification, sauceController.getAllSauces);
-router.get('/:id', authentification, sauceController.getOneSauce);
-router.put('/:id', authentification, multer, sauceController.modifySauce);
-router.delete('/:id', authentification, sauceController.deleteSauce);
-router.post('/:id/like', authentification, sauceController.likeSauce);
+router.post('/', authentication, multer, sauceController.createSauce);
+router.get('/', authentication, sauceController.getAllSauces);
+router.get('/:id', authentication, sauceController.getOneSauce);
+router.put('/:id', authentication, multer, sauceController.modifySauce);
+router.delete('/:id', authentication, sauceController.deleteSauce);
+router.post('/:id/like', authentication, sauceController.likeSauce);
 
 module.exports = router;
